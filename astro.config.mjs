1df5c563f1d6b81dd7035faf4fd64f2d8a824fbe
danielmicallef <daniel.micallef@aracor.ai>
2026-01-13 13:00:47 +0100

    chore: add name,sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: 'https://ekselera.com',
+  output: 'server',
+  base: '',
+  integrations: [sitemap()],
   adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
